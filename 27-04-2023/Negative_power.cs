c22d7677568cd41978b55b1e27f33781f38f3c32
anujavajja <115220049+anujavajja@users.noreply.github.com>
2023-05-10 18:18:21 +0530

    C# Programs

---
+++ 27-04-2023/Negative_power.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_basic_programs
+{
+    class Negative_power
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a  base number");
+            int basen=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter an exponent number");
+            int exp = Convert.ToInt32(Console.ReadLine());
+            double result = 1;
+            for(int i = 0; i>exp;i--)
+            {
+                result *= basen;
+            }
+            result = 1 / result;
+            Console.WriteLine(result);
+
+        }
+    }
+}
